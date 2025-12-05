118722aae2ab2f0e8d483b3142cd16a6733de73f
shantunu007 <chakmashantunu.web@gmail.com>
2025-12-05 23:24:05 +0600

    enable JSON parsing and add sample POST API

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,19 @@ import express, { Request, Response } from "express"
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World! boss, great to see you')
+});
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body);
+
+    res.status(201).json({
+        success: true,
+        message: "API is working"
+    })
 });
 
 export default app;
